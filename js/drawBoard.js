a33a2ee413d1d37f4753bc452cb9050b67016bda
Adam Johnson <adam.johnson@lucidworks.com>
2014-04-28 21:17:06 -0700

    Add button img

--- js/drawBoard.js
+++ js/drawBoard.js
@@ -1,7 +1,10 @@
 function drawBoard() {
 
 	drawCharacter();
+
+
 	var newCharacterButton;
-    newCharacterButton = game.add.button(700, 500, 'button', newCharacter, this, 2, 1, 0)
+    newCharacterButton = game.add.button(600, 400, 'button', newCharacter, this, 0, 0, 0)
+    newCharacterButton.name = 'newCharbtn'
 
 }
