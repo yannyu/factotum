2451680432de1282b481150de169b25a02c1c278
Adam Johnson <adam.johnson@lucidworks.com>
2014-04-28 20:42:16 -0700

    We have the sharpest pencils

--- js/drawBoard.js
+++ js/drawBoard.js
@@ -1,15 +1,6 @@
-
-
 function drawBoard() {
-	textDisplays = Array();
-    textDisplays[0] = game.add.text(32, 30, 'None');
-    textDisplays[1] = game.add.text(32, 150, 'None');
-    npcs = Array();
-    npcs[0] = new npc();
-
-    items = Array();
-    items[0] = new item();
-
-
+	drawCharacter();
+	var newCharacterButton;
+    newCharacterButton 
 
 }
---
+++ js/drawCharacter.js
@@ -0,0 +1,10 @@
+function drawCharacter(){
+	textDisplays = Array();
+    textDisplays[0] = game.add.text(32, 30, 'None');
+    textDisplays[1] = game.add.text(32, 150, 'None');
+    npcs = Array();
+    npcs[0] = new npc();
+
+    items = Array();
+    items[0] = new item();
+}
