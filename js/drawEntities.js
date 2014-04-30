2a991377f2084d404c9f688009a603dc4b3ed71e
dpsullivan <dennis.p.sullivan@gmail.com>
2014-04-29 21:09:58 -0700

    *: refactored NPC

--- js/drawEntities.js
+++ js/drawEntities.js
@@ -3,7 +3,7 @@ function drawEntities(){
     textDisplays[0] = game.add.text(32, 30, 'None');
     textDisplays[1] = game.add.text(32, 150, 'None');
     npcs = Array();
-    npcs[0] = new npc();
+    npcs[0] = new Npc();
 
     items = Array();
     items[0] = new Item();
--- models/NPC.js
+++ models/NPC.js
@@ -1,23 +1,30 @@
-random_npc_names = ["Andy", "Bobby", "Christina", "Dennis", "Elise", "Frances"];
-random_npc_class = ["Fighter", "Spellcaster", "Healer", "Runner"];
-random_npc_race = ["Human", "Elf", "Orc", "Goblin", "Dwarf"];
+Npc = function() {
 
-random_item_types = ["Axe", "Balloon", "Claw Hammer", "Dao", "Epee", "Fuschina", "Gauche"];
-random_item_prefixes = ["Shiny", "Dull", "Stupid", "Clever", "Useless", "Utile"];
-random_item_suffixes = ["of Fire", "of Ice", "of Bludgeoning", "of Anger", "of the Elements"]
-
-npc.prototype.getInfo = function() {
-    return this.name+'\n'+this.class+'\n'+this.race;
-}
-
-npc.prototype.randomize = function() {
-    this.name = random_npc_names[game.rnd.integerInRange(0,random_npc_names.length-1)];
-    this.class = random_npc_class[game.rnd.integerInRange(0,random_npc_class.length-1)];
-    this.race = random_npc_race[game.rnd.integerInRange(0,random_npc_race.length-1)];
-}
-
-function npc() {
     this.name = "default_name";
     this.class = "default_class";
     this.race = "default_race";
-}
+
+};
+
+//class vars
+    random_npc_names = ["Andy", "Bobby", "Christina", "Dennis", "Elise", "Frances"];
+    random_npc_class = ["Fighter", "Spellcaster", "Healer", "Runner"];
+    random_npc_race = ["Human", "Elf", "Orc", "Goblin", "Dwarf"];
+
+    random_item_types = ["Axe", "Balloon", "Claw Hammer", "Dao", "Epee", "Fuschina", "Gauche"];
+    random_item_prefixes = ["Shiny", "Dull", "Stupid", "Clever", "Useless", "Utile"];
+    random_item_suffixes = ["of Fire", "of Ice", "of Bludgeoning", "of Anger", "of the Elements"]
+
+Npc.prototype = {
+
+    getInfo : function() {
+        return this.name+'\n'+this.class+'\n'+this.race;
+    },
+
+    randomize : function() {
+        this.name = random_npc_names[game.rnd.integerInRange(0,random_npc_names.length-1)];
+        this.class = random_npc_class[game.rnd.integerInRange(0,random_npc_class.length-1)];
+        this.race = random_npc_race[game.rnd.integerInRange(0,random_npc_race.length-1)];
+    }
+
+};
