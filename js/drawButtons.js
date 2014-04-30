5178c8f3f8782718d3e6488f40ed8f99c8b16091
Yann Yu <yannyu84@gmail.com>
2014-04-29 19:46:21 -0700

    moving item and NPC into models, breaking out the buttons

---
+++ js/drawButtons.js
@@ -0,0 +1,8 @@
+function drawButtons(){
+	var newCharacterButton;
+	newCharacterButton = game.add.button(600, 400, 'button', newCharacter, this)
+	newCharacterButton.name = 'newCharbtn'
+	//var transferItem;
+	//transferItem = game.add.button(500, 400, 'button', playerShop.replaceItem(items[0]), this)
+	//transferItem.name = 'transferItembtn'
+}
--- js/drawGameBoard.js
+++ js/drawGameBoard.js
@@ -1,9 +1,6 @@
 function drawGameBoard() {
 
 	drawEntities();
-
-	var newCharacterButton;
-    newCharacterButton = game.add.button(600, 400, 'button', newCharacter, this)
-    newCharacterButton.name = 'newCharbtn'
+	drawButtons();
     drawShop();
 }
