fdbd7ceb67fbdc735025108e8c83bf0d63faf3b5
Adam Johnson <adam.johnson@lucidworks.com>
2014-05-03 19:31:25 -0700

    Figuring out how buttons work

--- js/drawMainScreen.js
+++ js/drawMainScreen.js
@@ -1,6 +1,25 @@
 function drawMainScreen() {
 
-	//drawMapButton();
+
+	var MapButton, InventoryButton, NPCButton;
+	MapButton = game.add.button(100, 200, 'button', OpenMap, this)
+	MapButton.name = 'MapButton';
+	InventoryButton = game.add.button(300, 200, 'button', OpenInventoryScreen, this)
+	InventoryButton.name = 'transferItembtn'
+	NPCButton = game.add.button(500, 200, 'button', OpenNPCScreen, this)
+	NPCButton.name = 'NPCScreen';
+
+}
+
+
+function OpenMap() {
+		//drawMapButton();
+}
+
+function OpenInventoryScreen() {
 	drawInventoryButton();
+}
+
+function OpenNPCScreen() {
 	//drawNPCButton();
 }
