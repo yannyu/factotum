d740748fed8d2a51ab3747da277c84bc12cbb4b5
Yann Yu <yannyu84@gmail.com>
2014-05-05 20:28:08 -0700

    Actually removing the file

--- js/drawEntities.js
+++
@@ -1,7 +0,0 @@
-function drawEntities(){
-    global_textDisplays[0] = game.add.text(32, 30, 'None');
-    global_textDisplays[1] = game.add.text(32, 150, 'None');
-    global_npcs[0] = new Npc();
-
-    global_entityItems[0] = new Item();
-}
