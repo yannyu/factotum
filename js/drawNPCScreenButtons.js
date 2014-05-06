6ab85a82201cc92c3ded0c69efb8c06016641f66
Yann Yu <yannyu84@gmail.com>
2014-05-05 19:46:55 -0700

    Adding and killing npc functionality

--- js/drawNPCScreenButtons.js
+++ js/drawNPCScreenButtons.js
@@ -8,6 +8,10 @@ function drawNPCScreenButtons(){
 	var addCharacterButton;
 	addCharacterButton = game.add.button(600, 200, 'butt', addNPC, this);
 	addCharacterButton.name = 'addCharacterBtn'
+	var killRandomCharacterButton;
+	killRandomCharacterButton = game.add.button(600, 300, 'butt', killRandomNPC, this);
+	killRandomCharacterButton.name = 'killRandomCharacterBtn'
+
 }
 
 function newNPC() {
@@ -25,4 +29,17 @@ function addNPC() {
 	global_npcs.push(new Npc());
 	global_npcs[global_npcs.length-1].create();
 	global_textDisplays[global_npcs.length-1] = game.add.text(32, 30+(global_npcs.length-1)*70,global_npcs[global_npcs.length-1].getInfo());
+}
+
+function killRandomNPC() {
+	for (i = 0; i < global_npcs.length; i++) {
+		global_textDisplays[i].setText('');
+	}
+
+	unluckyNPC = game.rnd.integerInRange(0,global_npcs.length-1);
+	global_npcs.splice(unluckyNPC, 1);
+
+	for (i = 0; i < global_npcs.length; i++) {
+		global_textDisplays[i].setText(global_npcs[i].getInfo());
+	}
 }
