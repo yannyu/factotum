613ddc006fc79a735cbc1f870fdd5904b737aee0
dpsullivan <dennis.p.sullivan@gmail.com>
2014-05-05 18:57:24 -0700

    Added inventory button images Added preloading to inventory state for inventory button images Added buttons to navigate shop inventory screen

--- js/inventory/drawInventory.js
+++ js/inventory/drawInventory.js
@@ -5,14 +5,31 @@ function drawInventory() {
 
 function drawTextBoxes() {
 	textDisplays = Array();
-	textDisplays[0] = game.add.text(32, 30, 'None');
-    textDisplays[1] = game.add.text(32, 150, 'None');
+	textDisplays[0] = game.add.text(32, 100, 'None');
+    textDisplays[1] = game.add.text(32, 200, 'None');
 
 }
 
 function drawInventoryButtons() {
 	var returnToMainButton;
-	returnToMainButton = game.add.button((game.width - 50), (game.height - 50), 'mainButton', returnToMain, this)
-	returnToMainButton.name = "returnToMainBtn"
+	returnToMainButton = game.add.button((game.width - 50), (game.height - 50), 'mainButton', returnToMain, this);
+	returnToMainButton.name = "returnToMainBtn";
+
+	var showInventoryButton;
+	showInventoryButton = game.add.button(50, 50, 'showInventoryButtonImage', returnToMain, this);
+	showInventoryButton.name = "showInventoryBtn";
+
+	var showResourcesButton;
+	showResourcesButton = game.add.button(200, 50, 'showResourcesButtonImage', returnToMain, this);
+	showResourcesButton.name = "showResourcesBtn";
+
+	var showSkillsButton;
+	showSkillsButton = game.add.button(350, 50, 'showSkillsButtonImage', returnToMain, this);
+	showSkillsButton.name = "showSkillsBtn";
+
+	var showShopButton;
+	showShopButton = game.add.button(450, 50, 'showShopButtonImage', returnToMain, this);
+	showShopButton.name = "showShopBtn";
+
 	
 }
